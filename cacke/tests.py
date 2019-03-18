60a79d44e6b39bb35394becd0bf787b58e899857
ah8ad3 <ah8ad3@gmail.com>
2019-03-18 22:45:29 +0330

    some tests added

--- cacke/tests.py
+++ cacke/tests.py
@@ -1,8 +1,20 @@
 from django.test import TestCase
 
 import conf
+import manage
+from cacke import wsgi
+from cacke.settings.celery import debug_task
 
 
 class Cacke(TestCase):
     def test_config_sphinx(self):
         self.assertEqual(conf.author, 'ah8ad3')
+
+    def test_wsgi_module(self):
+        self.assertIsNotNone(wsgi.application, 'check wsgi is not nil')
+
+    def test_manage_module(self):
+        self.assertIsNotNone(manage, 'check main manage is not nil')
+
+    def test_celery_app_task(self):
+        self.assertIsNotNone(debug_task.apply(), 'check task not empty')
